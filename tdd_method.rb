de85afdd9afcc7d7567f441e86ca4c67dbe0da83
aljarrell <alan.k.jarrell@gmail.com>
2018-10-23 11:28:52 -0400

    tested with a different string

--- tdd_method.rb
+++ tdd_method.rb
@@ -8,15 +8,19 @@ class My_test < Minitest::Test
   end
 
   def test_for_rindex_in_hello
-    assert_equal(0, index("hello",'h'))
+    assert_equal(0, index("hello", 'h'))
   end
 
   def test_for_rindex_one
-    assert_equal(1, index("hello",'e'))
+    assert_equal(1, index("hello", 'e'))
   end
 
   def test_for_what_position_is_o
-    assert_equal(4, index("hello",?o))
+    assert_equal(4, index("hello", ?o))
+  end
+
+  def test_with_different_string
+    assert_equal(0, index("hello world", 'h'))
   end
 
 
