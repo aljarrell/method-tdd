1e11135d3df4851a07e8971840f3ce6c1e7e9aea
aljarrell <alan.k.jarrell@gmail.com>
2018-10-24 21:50:36 -0400

    failing test for concat of two arrays

--- tdd_array_method.rb
+++ tdd_array_method.rb
@@ -13,6 +13,10 @@ class My_test < Minitest::Test
 
   def test_for_one_in_array
     assert_equal([1], [1])
+  end
+
+  def test_for_concat_arrays
+    assert_equal([1, 2], glue([1], [2]))
   end 
 
 
