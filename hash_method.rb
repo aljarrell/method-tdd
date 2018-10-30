b597628b57ef47f522a8a50160d8386c428597de
aljarrell <alan.k.jarrell@gmail.com>
2018-10-30 14:40:33 -0400

    merge function working properly

--- hash_method.rb
+++ hash_method.rb
@@ -1,8 +1,10 @@
 #Hash methods merge and rehash
 
 def merge(h1, h2)
-  {}
-
+  h2.each do |item|
+    h1[item[0]] = item[1]
+  end
+return h1 
 
 end
 
--- tdd_hash_method.rb
+++ tdd_hash_method.rb
@@ -23,4 +23,16 @@ class My_test < Minitest::Test
     assert_equal({1 => "A", 2 => "B"}, merge(h1, h2))
   end
 
+  def test_for_multiple_elements
+    h1 = {1 => "a", 2 => "b"}
+    h2 = {3 => "c", 4 => "d"}
+    assert_equal({1 => "a", 2 => "b", 3 => "c", 4 => "d"}, merge(h1, h2))
+  end
+
+  def test_for_update
+    h1 = {1 => "a"}
+    h2 = {1 => "b"}
+    assert_equal({1 => "b"}, merge(h1, h2))
+  end
+
 end
